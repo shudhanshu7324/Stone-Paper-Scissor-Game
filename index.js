71a97ec56e517d06c7360ce19e3fcaf953ed5d33
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-08-21 23:52:26 +0530

    added js logic

--- index.js
+++ index.js
@@ -22,3 +22,29 @@ const urScore = document.getElementsByClassName('ur-score');
 
 cmpScore[0].innerText = computerScore;
 urScore[0].innerText = yourScore;
+
+const computer = ["paper","scissor","rock"];
+
+function computerSelect(){
+    const computerResult = computer[Math.floor(Math.random()*3)];
+    return computerResult;
+}
+
+
+const buttons = document.querySelectorAll('.icon');
+
+buttons.forEach(button=>{
+    button.addEventListener('click', function(){
+        const result = computerSelect();
+        if(result === this.id){
+            console.log("tie");   
+        } else{
+            console.log("won");   
+        }
+    })
+})
+
+
+// console.log(result);
+
+
